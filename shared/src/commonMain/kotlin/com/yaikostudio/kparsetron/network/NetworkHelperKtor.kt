734a53534f5c8e3c6d5986f93dbe7a48e55ace2e
Antonio Nicolás Pina <antonio@yaikostudio.com>
2025-03-12 08:37:40 +0100

    Allow cookie handling.

--- shared/src/commonMain/kotlin/com/yaikostudio/kparsetron/network/NetworkHelperKtor.kt
+++ shared/src/commonMain/kotlin/com/yaikostudio/kparsetron/network/NetworkHelperKtor.kt
@@ -1,9 +1,12 @@
 package com.yaikostudio.kparsetron.network
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.cookies.HttpCookies
+import io.ktor.client.plugins.cookies.cookies
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.request
 import io.ktor.http.Url
 
 class NetworkHelperKtor(
@@ -13,8 +16,11 @@ class NetworkHelperKtor(
         val instance = NetworkHelperKtor(
             HttpClient(provideHttpClientEngine()) {
                 this.followRedirects = true
+                install(HttpCookies)
             },
         )
+
+        suspend fun HttpResponse.getCookies() = instance.client.cookies(request.url)
     }
 
     suspend fun get(
