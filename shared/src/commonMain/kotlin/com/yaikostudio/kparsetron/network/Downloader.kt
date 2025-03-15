f146d63f4893ef3d2c026d5fe8cc0204c55dacc5
Antonio Nicolás Pina <antonio@yaikostudio.com>
2025-03-15 13:09:50 +0100

    Simplify downloader.

--- shared/src/commonMain/kotlin/com/yaikostudio/kparsetron/network/Downloader.kt
+++ shared/src/commonMain/kotlin/com/yaikostudio/kparsetron/network/Downloader.kt
@@ -3,7 +3,6 @@ package com.yaikostudio.kparsetron.network
 import com.fleeksoft.ksoup.Ksoup
 import com.fleeksoft.ksoup.nodes.Document
 import com.fleeksoft.ksoup.parser.Parser
-import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.header
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
@@ -11,38 +10,23 @@ import io.ktor.client.statement.request
 import io.ktor.http.Url
 
 class Downloader {
-    private fun HttpRequestBuilder.addHeaderIfNotPresent(
-        name: String,
-        value: String,
-    ) {
-        if (null == headers[name]) {
-            header(name, value)
-        }
-    }
-
     suspend fun get(
         url: Url,
         referrer: Url? = null,
-        headers: Map<String, String> = emptyMap(),
+        extraHeaders: Map<String, String> = emptyMap(),
     ): HttpResponse {
         return NetworkHelperKtor.instance.get(
             url = url,
             httpRequestBuilder = {
-                headers.forEach { (key, value) ->
-                    header(key, value)
-                }
-                addHeaderIfNotPresent(
-                    "Accept",
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-                )
-                addHeaderIfNotPresent("Accept-Language", "en-us,en;q=0.5")
-                addHeaderIfNotPresent("Upgrade-Insecure-Requests", "1")
-                addHeaderIfNotPresent(
-                    "User-Agent",
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
-                )
+                header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                header("Accept-Language", "en-us,en;q=0.5")
+                header("Upgrade-Insecure-Requests", "1")
+                header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36")
                 if (null != referrer) {
-                    addHeaderIfNotPresent("Referer", referrer.toString())
+                    header("Referer", referrer.toString())
+                }
+                extraHeaders.forEach { (key, value) ->
+                    headers[key] = value
                 }
             },
         )
